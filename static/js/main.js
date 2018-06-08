c996d29350201ed041726d88c30ded3a86d00a1c
Evgeny Chistyakov <just15@yandex.ru>
2018-06-08 19:18:31 +0300

    Первый коммит

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {  
+    $('.slider__list').bxSlider({
+        slideWidth: 650,
+        pager: false,
+        nextText: '',
+        nextSelector: '.slider__next',
+        prevSelector: '.slider__prev',
+        prevText: ''
+    });
+});
+
